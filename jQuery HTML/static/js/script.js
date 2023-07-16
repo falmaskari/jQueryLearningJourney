00280b6f8ba9a335cdabffd181a6df045600944b
Fatma Almaskari <130557093+falmaskari@users.noreply.github.com>
2023-07-16 18:28:14 +0400

    Add files via upload

---
+++ jQuery HTML/static/js/script.js
@@ -0,0 +1,9 @@
+$("#color").click(function(){
+    $("#color").css({"color":"red"});
+})
+
+$("#square").click(function(){
+   
+    $('#square').html('width of Square :'  + $("#square").width() + '<br>' + 'height of Square :'  + $("#square").height())
+
+})
